ddf752f12db97f8b0f74beb5b26c69b9b7f0fadf
SalmaaaTareeek <salmaaatareeek97@gmail.com>
2020-11-08 01:43:58 +0200

    accessing json data without saving in local dir

--- access.py
+++ access.py
@@ -1,10 +1,13 @@
 import requests
+import json
+import os
 import urllib.request
+import urllib
 BASE = "https://api.github.com/search/repositories?q=created:%3E2020-10-07&sort=stars&order=desc" #can be changeable
-file = open("api.json", "w" , encoding='utf-8')
-for line in urllib.request.urlopen(BASE):
-    readable = line.decode('utf-8')
-    file.write(readable+"\n")
-    file.close()
+response = urllib.request.urlopen(BASE)
+data = json.loads(response.read())
+print(data)
+
+
 
 
