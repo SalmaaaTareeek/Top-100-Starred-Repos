2284d9106aacf072cf9b334fe6a73da772a076ff
SalmaaaTareeek <salmaaatareeek97@gmail.com>
2020-11-08 20:07:28 +0200

    adding on main server code for parsing

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from flask import Flask,request
 from flask_restful import Api, Resource,reqparse,abort
 import requests
-
+import urllib.request
 #  to create an app using flask
 app = Flask(__name__)
 api = Api(app)
+#For parsing data in json file exported from the github api
+repo_add_args = reqparse.RequestParser()
+repo_add_args.add_argument("name",type=str , help="" , required=True)
+repo_add_args.add_argument("language",type=str , help="",required=True)
+repo_add_args.add_argument("repo_id",type=int , help="",required=True)
+github_data = {}
+class repos(Resource):
+    def get(self,items,repo_id,language):
+        return github_data[items][repo_id][language]
 
-
-# api.add_resource(Video,"/video/<int:video_id>")
+api.add_resource(repos,"/api/<int:repo_id>")
 
 
 
