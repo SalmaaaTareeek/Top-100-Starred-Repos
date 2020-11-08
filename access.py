cbc4fd6b0979b21795e869dd3a82b5c4c0f6f9ae
SalmaaaTareeek <salmaaatareeek97@gmail.com>
2020-11-09 01:14:41 +0200

    adding number of users using specfic languages in dict

--- access.py
+++ access.py
@@ -4,21 +4,31 @@ import os
 import urllib.request
 import urllib
 BASE = "http://127.0.0.1:5000/"
-url = "https://api.github.com/search/repositories?q=created:%3E2020-10-08&sort=stars&order=desc" #can be changeable
-max = 0
-count = 0
+url = "https://api.github.com/search/repositories?q=created:%3E2020-10-09&sort=stars&order=desc" #can be changeable
 response = urllib.request.urlopen(url)
 data = json.loads(response.read())
 json_data_from_github_api = {}
 list_of_names_and_languages = {"name":[],"language":[]}
 list_of_languages = []
-for i in range(len(data["items"])-1):
+list_of_unique_langs_and_users={"language":[],"users":[]}
+for i in range(len(data["items"])):
     json_data_from_github_api = requests.get(BASE + "/api/" + str(i) + "/" + str(data["items"][i]["name"]) + "/" + str(data["items"][i]["language"]))
     list_of_names_and_languages["name"].append(json_data_from_github_api.json()['name'])
     list_of_names_and_languages["language"].append(json_data_from_github_api.json()['language'])
 for j in range(len(list_of_names_and_languages["language"])):
     list_of_languages.append(list_of_names_and_languages["language"][j])
-#print(list_of_languages)
+for k in list_of_languages:
+    if k in list_of_unique_langs_and_users["language"]:
+        continue
+    else:
+        list_of_unique_langs_and_users["language"].append(k)
+        list_of_unique_langs_and_users["users"].append(list_of_languages.count(k))
+print(list_of_unique_langs_and_users)
+
+
+
+
+
 
 
 
