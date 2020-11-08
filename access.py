44bc0d5cb2e7f3dc7e081b81e3d12c0f246ed04f
SalmaaaTareeek <salmaaatareeek97@gmail.com>
2020-11-08 22:54:18 +0200

    adding the names and languages from json to dict

--- access.py
+++ access.py
@@ -8,11 +8,15 @@ url = "https://api.github.com/search/repositories?q=created:%3E2020-10-08&sort=s
 response = urllib.request.urlopen(url)
 data = json.loads(response.read())
 json_data_from_github_api = {}
+list_of_names_and_languages = {"name":[],"language":[]}
 print(data["items"][0]["language"]) #accessing specfic Language
 print(type(data["items"][0]["language"]))
 for i in range(len(data["items"])-1):
-    json_data_from_github_api = requests.get(BASE + "/api/" + str(i) + "/" + str(data["items"][i]["name"]) + "/" + str(data["items"][i]["language"]) )
-#print(json_data_from_github_api.json()['name'])
+    json_data_from_github_api = requests.get(BASE + "/api/" + str(i) + "/" + str(data["items"][i]["name"]) + "/" + str(data["items"][i]["language"]))
+    list_of_names_and_languages["name"].append(json_data_from_github_api.json()['name'])
+    list_of_names_and_languages["language"].append(json_data_from_github_api.json()['language'])
+
+print(list_of_names_and_languages)
 
 
 
