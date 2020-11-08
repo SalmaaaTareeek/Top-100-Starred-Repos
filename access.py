26a4de0939f3c563e0aea38b052de90be8f30313
SalmaaaTareeek <salmaaatareeek97@gmail.com>
2020-11-09 00:22:54 +0200

    adding list of languages

--- access.py
+++ access.py
@@ -5,18 +5,25 @@ import urllib.request
 import urllib
 BASE = "http://127.0.0.1:5000/"
 url = "https://api.github.com/search/repositories?q=created:%3E2020-10-08&sort=stars&order=desc" #can be changeable
+max = 0
+count = 0
 response = urllib.request.urlopen(url)
 data = json.loads(response.read())
 json_data_from_github_api = {}
 list_of_names_and_languages = {"name":[],"language":[]}
-print(data["items"][0]["language"]) #accessing specfic Language
-print(type(data["items"][0]["language"]))
+list_of_languages = []
 for i in range(len(data["items"])-1):
     json_data_from_github_api = requests.get(BASE + "/api/" + str(i) + "/" + str(data["items"][i]["name"]) + "/" + str(data["items"][i]["language"]))
     list_of_names_and_languages["name"].append(json_data_from_github_api.json()['name'])
     list_of_names_and_languages["language"].append(json_data_from_github_api.json()['language'])
+for j in range(len(list_of_names_and_languages["language"])):
+    list_of_languages.append(list_of_names_and_languages["language"][j])
+#print(list_of_languages)
 
-print(list_of_names_and_languages)
+
+
+
+
 
 
 
