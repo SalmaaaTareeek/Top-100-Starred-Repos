c1140118cacc6369ece33a0d5a7ebf1f955a5bb8
SalmaaaTareeek <salmaaatareeek97@gmail.com>
2020-11-10 15:14:25 +0200

    V.1

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ app = Flask(__name__)
 api = Api(app)
 github_data = {}
 class repos(Resource):
+    #this function will get the repo_id which will be initialized with 0
+    #name will got the name of the repo
+    #language will got the language used in repos
     def get(self,repo_id,name,language):
         return {"ID":repo_id,"name":name,"language":language}
-
+#will pass the repo ID and name and language to the class and then this will get accessed by access.py
 api.add_resource(repos,"/api/<string:repo_id>/<string:name>/<string:language>")
-# to run the application
+# Initializing the server
 if __name__ == "__main__":
-    app.run(debug=True)  #debuggind mode menans testing mode as a developer not a production
+    app.run(debug=False)  #debuggind mode menans testing mode as a developer not a production
 
 
 
